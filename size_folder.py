90ac8e38e63a813aadffda721eab879af3b83a33
Y57B <rafaelpaz@petrobras.com.br>
2020-01-29 10:59:32 -0300

    Show size of files and subfolders to a given folder

--- size_folder.py
+++ size_folder.py
@@ -1,5 +1,9 @@
 import os
 import argparse
+#from progressbar import ProgressBar
+
+def size_file(file_path):
+    return os.path.getsize(file_path)/1000000
 
 def size_folder(folder):
     total_size = 0
@@ -7,8 +11,8 @@ def size_folder(folder):
         for f in files:
             file_path = os.path.join(root, f)
             if not os.path.islink(file_path):
-                total_size += os.path.getsize(file_path)
-    return total_size/1000000
+                total_size += size_file(file_path)
+    return total_size
 
 
 if __name__ == "__main__":
@@ -20,14 +24,30 @@ if __name__ == "__main__":
     subdirs = []
     total = 0
 
-    for root, dirs, files in os.walk(PATH):    
+    for root, dirs, files in os.walk(PATH):        
         for d in dirs:
             subdirs.append(os.path.join(root, d))
         break
-
+    
+    #pb = ProgressBar(len(subdirs))
+    result = []
     for d in subdirs:
+        #pb.next()
         sf = size_folder(d)
         total += sf
-        print('{0} \t\t\t {1:.2f} Mb'.format(d, sf))
+        result.append((d, sf))
+        #print('{0} \t\t\t {1:.2f} Mb'.format(d, sf))
     
+    for f in os.listdir(root):
+        file_path = os.path.join(root, f)
+        if os.path.isfile(file_path):
+            sf = size_file(file_path)
+            total += sf
+            result.append((file_path, sf))
+            
+    result.sort(key=lambda x: x[1], reverse=True)
+
+    for r in result:
+        print('{0} \t\t\t {1:.2f} Mb'.format(r[0], r[1]))
+
     print('Total: {0:.2f} Mb'.format(total))
